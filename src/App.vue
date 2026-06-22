6afca166e261b751bf51f60d34f64d492fd7096f
Archameuse <divulinm@gmail.com>
2026-06-22 22:34:45 +0300

    update: changed name policy mihail -> mikhail

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,25 @@
 </template>
 
 <script setup lang="ts">
+import { computed, watch } from 'vue'
 import AboutSection from './components/AboutSection.vue'
 import HeroSection from './components/HeroSection.vue'
 import ProjectsSection from './components/ProjectsSection.vue'
 import TheFooter from './components/TheFooter.vue'
 import TheHeader from './components/TheHeader.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const dynamicTitle = computed(() => {
+  return `${t('firstname')} ${t('lastname')}`
+})
+
+watch(
+  dynamicTitle,
+  (newTitle) => {
+    document.title = newTitle
+  },
+  { immediate: true },
+)
 </script>
